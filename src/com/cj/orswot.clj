819f62d0d9ded1a8b78a98788309117d243a5b59
Fedotov Daniil <hairyhum@gmail.com>
2015-10-05 14:53:24 +0300

    Proper orswot

--- src/com/cj/orswot.clj
+++ src/com/cj/orswot.clj
@@ -1,58 +1,48 @@
 (ns com.cj.orswot
-  (:refer-clojure :exclude [get remove])
+  (:refer-clojure :exclude [remove])
   (:require [com.cj.vv :as vv])
   (:use [com.cj.crdt]))
 
-
-(defrecord VersionSet [versions actor]
-  StateCRDT
-  (query-dt [vset] (keys (:versions vset)))
-  (update-dt [vset [action value]]
-    (let [version (#'clojure.core/get (:versions vset) value)]
-      (case action
-        :add
-          (if version
-            (update vset :versions assoc value (vv/increment version actor 1))
-            (update vset :versions assoc value (vv/new {actor 1})))
-        :remove vset)))
-  (merge-dt [ours theirs]
-    (assoc ours :version (merge-with vv/merge (:versions ours) (:versions theirs)))))
-
-(defrecord Orswot [structure data]
+; Data is map of value:dot
+(defrecord Orswot [version data actor]
   StateCRDT
   (query-dt [orswot]
-    (select-keys data (query-dt (:structure orswot))))
+    (select-keys (:data orswot)))
   (update-dt [orswot [action value]]
-    {:pre (contains? value :id)}
     (case action
       :add 
-        (let [id (:id value)
-              new-structure (update-dt (:structure orswot) [:add id])
-              new-data (assoc (:data orswot) id value)]
-          (assoc orswot :data new-data :structure new-structure))
+        (let [actor (:actor orswot)
+              new-version  (vv/increment (:version orswot) actor)
+              item-dot (get actor new-version)
+              new-data (assoc (:data orswot) value item-dot)]
+          (assoc orswot :data new-data :version new-version))
       :remove
-        (let [id (:id value)
-              new-structure (update-dt (:structure orswot) [:remove id])
-              new-data (dissoc (:data orswot) id)]
-          (assoc orswot :data new-data :structure new-structure))))
+        (update orswot :data dissoc value)))
   (merge-dt [our their]
-    (let [new-structure (merge-dt (:structure our) (:structure their))
-          new-data (select-keys data (query-dt new-structure))]
-      (Orswot. new-structure new-data))))
+    (let [new-version (vv/merge (:version our) (:version their))
+          our-data (reduce-kv (fn [acc key val]
+                (let [their-val (get (:data their) key)]
+                  (if their-val
+                    (assoc acc key (vv/merge val their-val))
+                    (if (dot-greater val (:version their))
+                      (assoc acc key val)
+                      acc)))))
+          their-data (reduce-kv (fn [acc key val]
+                  (if (get (:data our) key)
+                    acc
+                    (if (dot-greater val (:version our))
+                      (assoc acc key val)
+                      acc))))]
+          (assoc our :data (merge our-data their-data) :version new-version))))
 
 (defn new [actor] 
-  (Orswot. (VersionSet. {} actor) {}))
+  (Orswot. (vv/new) {} actor))
 
 (defn add [orswot value]
-  {:pre (contains? value :id)}
   (update-dt orswot [:add value]))
 
 (defn remove [orswot value]
-  {:pre (contains? value :id)}
   (update-dt orswot [:remove value]))
 
-(defn get [orswot key]
-  (#'clojure.core/get (query-dt orswot) key))
-
-(defn update [orswot key value]
-  (update orswot :data assoc key value))
+(defn contains? [orswot value]
+  (core/contains? (query-dt orswot) value))
--- src/com/cj/vv.clj
+++ src/com/cj/vv.clj
@@ -6,10 +6,14 @@
 (defn summ-all [vv]
   (reduce-kv (fn [acc _ val] (+ acc val)) 0 vv))
 
-(defn increment [vv actor val]
-  (if (contains? vv actor)
-    (update vv actor + val)
-    (conj vv {actor val})))
+(defn increment 
+  [vv actor]
+    (recur vv actor 1)
+  [vv actor val]
+    (if (contains? vv actor)
+      (update vv actor + val)
+      (conj vv {actor val})))
 
 (defn merge [our their]
   (merge-with (fn [acc el] (max acc el)) our their))
+
